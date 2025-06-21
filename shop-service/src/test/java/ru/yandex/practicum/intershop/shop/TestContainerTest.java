74689ef0c69e30d22961dedaec234d1aaeffb1b5
Fedor MItyanin <79231292918@ya.ru>
2025-06-21 18:10:20 +0700

    add redis to test

--- shop-service/src/test/java/ru/yandex/practicum/intershop/shop/TestContainerTest.java
+++ shop-service/src/test/java/ru/yandex/practicum/intershop/shop/TestContainerTest.java
@@ -9,6 +9,7 @@ import org.springframework.r2dbc.connection.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -36,13 +37,23 @@ public class TestContainerTest {
             .withInitScript("schema.sql")
             .withReuse(true);
 
+    @Container
+    public static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.2")
+            .withExposedPorts(6379)
+            .withReuse(true);
+
     @DynamicPropertySource
     private static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        // PostgreSQL (R2DBC)
         registry.add("spring.r2dbc.url", () ->
                 postgreSQLContainer.getJdbcUrl().replace("jdbc:", "r2dbc:")
         );
         registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername);
         registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword);
+
+        // Redis
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
     }
 
     protected void executeSqlScriptsBlocking(List<String> sqlScripts) {
