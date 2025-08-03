e7447e737cf8632fc9d4a84db1c81e37b38d16a6
Fedor MItyanin <79231292918@ya.ru>
2025-08-03 21:14:21 +0700

    add keycloak

--- shop-service/src/main/java/ru/yandex/practicum/intershop/shop/config/PaymentClientConfig.java
+++ shop-service/src/main/java/ru/yandex/practicum/intershop/shop/config/PaymentClientConfig.java
@@ -4,6 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.yandex.practicum.intershop.payment.client.ApiClient;
 import ru.yandex.practicum.intershop.payment.client.api.PaymentApi;
 
@@ -15,9 +22,36 @@ public class PaymentClientConfig {
     private String paymentApiUrl;
 
     @Bean
-    public ApiClient paymentApiClient() {
+    ReactiveOAuth2AuthorizedClientManager auth2AuthorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ReactiveOAuth2AuthorizedClientService authorizedClientService
+    ) {
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
+
+        manager.setAuthorizedClientProvider(ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .refreshToken()
+                .build()
+        );
+        return manager;
+    }
+
+    @Bean
+    WebClient paymentWebClient(ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
+        var oauth2Filter = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Filter.setDefaultClientRegistrationId("keycloak");
+
+        return WebClient.builder()
+                .baseUrl(paymentApiUrl)
+                .filter(oauth2Filter)
+                .build();
+    }
+
+    @Bean
+    public ApiClient paymentApiClient(WebClient webClient) {
         log.info("paymentApiUrl: {}", paymentApiUrl);
-        ApiClient client = new ApiClient();
+        ApiClient client = new ApiClient(webClient);
         client.setBasePath(paymentApiUrl);
         return client;
     }
